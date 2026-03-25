f57b7a8503cfbaeac95664718f37acff28352d8b
Ellie Ford <eford@tvscientific.com>
2026-03-24 17:31:50 -0700

    use libunwind for backtraces

--- build.zig
+++ build.zig
@@ -88,6 +88,7 @@ pub fn build(b: *std.Build) void {
             "-std=c++17",
         },
     });
+    tracy.root_module.addCMacro("TRACY_LIBUNWIND_BACKTRACE", "");
 
     if (options.enable_ztracy) tracy.root_module.addCMacro("TRACY_ENABLE", "");
     if (options.enable_fibers) tracy.root_module.addCMacro("TRACY_FIBERS", "");
